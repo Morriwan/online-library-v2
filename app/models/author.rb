21020dfc85e82a7ba137bdeab23bc2d5e4075c3f
Baptiste Garrigues <garrigues.baptiste@gmail.com>
2024-05-06 12:16:50 +0200

    Mise en place

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
   has_many :genres, through: :books
   has_many :categories, through: :books
-  validates :name, presence: true
+  validates :first_name, :last_name, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,25 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Cleaning up database..."
+Book.destroy_all
+Author.destroy_all
+Category.destroy_all
+Genre.destroy_all
+puts "Database cleaned up!"
+
+puts "Creating library..."
+author = Author.create!(first_name: "F. Scott", last_name: "Fitzgerald")
+category = Category.create!(name: "Fiction")
+genre = Genre.create!(name: "Classic")
+Book.create!(
+  name: "The Great Gatsby",
+  isbn: "9780743273565",
+  year: 1925,
+  author: author,
+  category: category,
+  genre: genre
+)
+
+puts "Library created!"
